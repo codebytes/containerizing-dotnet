5cf5cbf03b63c62728b426b7bf24a40b21e1c24a
Chris Ayers <clayers@gmail.com>
2024-01-16 21:25:31 +0000

    Add runtime information to program output

--- src/hello-containers/Program.cs
+++ src/hello-containers/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Runtime.InteropServices;
+
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("FrameworkDescription: " + RuntimeInformation.FrameworkDescription);
+Console.WriteLine("OSArchitecture: " + RuntimeInformation.OSArchitecture);
+Console.WriteLine("OSDescription: " + RuntimeInformation.OSDescription);
+Console.WriteLine("ProcessArchitecture: " + RuntimeInformation.ProcessArchitecture);
+Console.WriteLine("RuntimeIdentifier: " + RuntimeInformation.RuntimeIdentifier);
